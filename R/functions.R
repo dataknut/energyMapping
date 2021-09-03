55e654447916a92e18e3c55b3c110b1793693eea
Ben Anderson <dataknut@icloud.com>
2021-09-03 14:41:25 +0100

    amended getSoton to not include Rushmoor

--- R/functions.R
+++ R/functions.R
@@ -6,6 +6,7 @@
 getSolent <- function(df){
   # function to return the rows that match on Local Authority name
   # assumes we're matching on la_name
+  # this aligns with the .shp boundary file we pre-created
   solent <- dplyr::filter(df, la_name == "Basingstoke and Deane"|
                             la_name == "East Hampshire"|
                             la_name == "Eastleigh"|
@@ -15,7 +16,7 @@ getSolent <- function(df){
                             la_name == "Havant"|
                             la_name == "New Forest"|
                             la_name == "Portsmouth"|
-                            la_name == "Rushmoor"| # northern Hampshire - not always considered part of Solent
+                            #la_name == "Rushmoor"| # northern Hampshire - not always considered part of Solent
                             la_name == "Southampton"|
                             la_name == "Test Valley"|
                             la_name == "Winchester"|
